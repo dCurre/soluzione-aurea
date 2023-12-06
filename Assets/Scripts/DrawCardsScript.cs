1ed0825aaa68379c873815ede5efa2ff1a9026c8
dCurre <davidecurreli@gmail.com>
2023-12-06 16:53:50 +0100

    Background + player areas

---
+++ Assets/Scripts/DrawCardsScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawCardsScript : MonoBehaviour
+{
+
+    public GameObject card1, card2, playerArea, secondPlayerArea;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void onClick(){
+        //GameObject playerCard = Instanciate(card1, new Vector3);
+    }
+}
